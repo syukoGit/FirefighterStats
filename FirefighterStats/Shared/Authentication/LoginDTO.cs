80bb554168884d78d7cf2ab93659b96a652885e0
syukoGit <syukodev@outlook.com>
2023-01-03 01:15:07 +0100

    Remove ConfirmPassword on LoginDTO

--- FirefighterStats/Shared/Authentication/LoginDTO.cs
+++ FirefighterStats/Shared/Authentication/LoginDTO.cs
@@ -11,15 +11,10 @@ using System.ComponentModel.DataAnnotations;
 public class LoginDTO
 {
     [Required]
-    [Compare(nameof(Password))]
-    [DataType(DataType.Password)]
-    public string ConfirmPassword { get; set; } = string.Empty;
-
-    [Required]
     [DataType(DataType.Password)]
     public string Password { get; set; } = string.Empty;
 
-    public bool RememberMe { get; set; } = false;
+    public bool RememberMe { get; set; }
 
     [Required]
     public string UserName { get; set; } = string.Empty;
