b777075716fff7c00f3f24a9a687df5f38e20137
syukoGit <syukodev@outlook.com>
2023-01-06 16:24:17 +0100

    Add ValidationAttributes * BeforeNow * RequiresDigit * RequiresLowercaseLetter * RequiresNonAlphanumeric * RequiresUppercaseLetter

---
+++ FirefighterStats/Shared/ValidationAttributes/RequiresDigitAttribute.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.Shared" file="RequiresDigitAttribute.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Shared.ValidationAttributes;
+
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+[AttributeUsage(AttributeTargets.Property)]
+public partial class RequiresDigitAttribute : ValidationAttribute
+{
+    /// <inheritdoc />
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        return value is not string str || RegexPattern().IsMatch(str)
+                   ? ValidationResult.Success
+                   : new ValidationResult("Password must be contains a digit.");
+    }
+
+    [GeneratedRegex(@"\d")]
+    private static partial Regex RegexPattern();
+}
