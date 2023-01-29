17dea76e3690c290b5cca050dbafa806b32ae91b
syukoGit <syukodev@outlook.com>
2023-01-29 16:34:22 +0100

    Add a service to manage the notification

---
+++ FirefighterStats/Client/Services/NotificationService.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.Client" file="NotificationService.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Client.Services;
+
+public class NotificationService
+{
+    private readonly Queue<string> _notifications = new ();
+
+    public bool HasNotification => _notifications.Count > 0;
+
+    public string NextNotification => _notifications.Dequeue();
+
+    public event EventHandler? NewNotification;
+
+    public void Add(string message)
+    {
+        _notifications.Enqueue(message);
+
+        NewNotification?.Invoke(this, EventArgs.Empty);
+    }
+}
