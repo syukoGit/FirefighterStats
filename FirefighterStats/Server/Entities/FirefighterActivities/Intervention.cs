fbac0aaba1dd2e7b4af3d8920c61aaf337ca0fb8
syukoGit <syukodev@outlook.com>
2023-02-02 17:13:24 +0100

    Create Intervention entity and some unit tests on the entity

---
+++ FirefighterStats/Server/Entities/FirefighterActivities/Intervention.cs
@@ -0,0 +1,100 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.Server" file="Intervention.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Server.Entities.FirefighterActivities;
+
+using FirefighterStats.Shared.IndemnitySlip.FirefighterActivities;
+using FirefighterStats.Shared.Utils;
+using JetBrains.Annotations;
+
+[UsedImplicitly]
+public class Intervention : FirefighterActivity
+{
+    private static readonly Percentage s_dayRate = "100%";
+
+    private static readonly Percentage s_nightRate = "200%";
+
+    private static readonly Percentage s_specialRate = "150%";
+
+    private DateTime _endDateTime = DateTime.MinValue;
+
+    private DateTime _startDateTime = DateTime.MinValue;
+
+    /// <inheritdoc />
+    public override double Amount => Math.Round((DayHours * s_dayRate + NightHours * s_nightRate + SpecialHours * s_specialRate) * UnitAmount, 2);
+
+    public double DayHours { get; private set; }
+
+    /// <inheritdoc />
+    public override required DateTime EndDateTime
+    {
+        get => _endDateTime;
+
+        set
+        {
+            _endDateTime = value;
+            CalculateHours();
+        }
+    }
+
+    public required EInterventionType InterventionType { get; set; }
+
+    public double NightHours { get; private set; }
+
+    public required int Number { get; set; }
+
+    public double SpecialHours { get; private set; }
+
+    /// <inheritdoc />
+    public override required DateTime StartDateTime
+    {
+        get => _startDateTime;
+
+        set
+        {
+            _startDateTime = value;
+            CalculateHours();
+        }
+    }
+
+    public double TotalHours => DayHours + NightHours + SpecialHours;
+
+    private void CalculateHours()
+    {
+        if (StartDateTime == DateTime.MinValue || EndDateTime == DateTime.MinValue)
+        {
+            return;
+        }
+
+        var nightMinutes = 0;
+        var specialMinutes = 0;
+        var dayMinutes = 0;
+
+        DateTime start = StartDateTime;
+
+        while (start < EndDateTime)
+        {
+            if (start.Hour is < 7 or >= 22)
+            {
+                nightMinutes++;
+            }
+            else if (start.DayOfWeek == DayOfWeek.Sunday)
+            {
+                specialMinutes++;
+            }
+            else
+            {
+                dayMinutes++;
+            }
+
+            start = start.AddMinutes(1);
+        }
+
+        NightHours = Math.Round(nightMinutes / 60.0, 2);
+        SpecialHours = Math.Round(specialMinutes / 60.0, 2);
+        DayHours = Math.Round(dayMinutes / 60.0, 2);
+    }
+}
---
+++ FirefighterStats/UnitTest/Server/Entities/Intervention_Test.cs
@@ -0,0 +1,63 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.UnitTest" file="Intervention_Test.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.UnitTest.Server.Entities;
+
+using System.Globalization;
+using FirefighterStats.Server.Entities.FirefighterActivities;
+using FirefighterStats.Shared.IndemnitySlip.FirefighterActivities;
+
+public class Intervention_Test
+{
+    private const string DateTimeFormat = "g";
+
+    [Theory]
+    [InlineData("06/11/2020 19:39", "06/11/2020 21:54", 17.82)]
+    [InlineData("07/11/2020 03:41", "07/11/2020 06:00", 36.75)]
+    [InlineData("18/10/2020 14:25", "18/10/2020 16:49", 28.51)]
+    [InlineData("29/11/2020 05:37", "29/11/2020 07:45", 30.77)]
+    public void Amount_InlineData_Success(string startStr, string endStr, double amount)
+    {
+        DateTime start = DateTime.ParseExact(startStr, DateTimeFormat, CultureInfo.CurrentCulture);
+        DateTime end = DateTime.ParseExact(endStr, DateTimeFormat, CultureInfo.CurrentCulture);
+
+        Intervention intervention = GetIntervention(start, end);
+
+        Assert.Equal(amount, intervention.Amount);
+    }
+
+    [Theory]
+    [InlineData("06/11/2020 19:39", "06/11/2020 21:54", 2.25, 0, 0, 2.25)]
+    [InlineData("07/11/2020 03:41", "07/11/2020 06:00", 0, 2.32, 0, 2.32)]
+    [InlineData("18/10/2020 14:25", "18/10/2020 16:49", 0, 0, 2.40, 2.40)]
+    [InlineData("29/11/2020 05:37", "29/11/2020 07:45", 0, 1.38, 0.75, 2.13)]
+    public void CheckHours_InlineData_Success(string startStr, string endStr, double dayHours, double nightHours, double specialHours, double totalHours)
+    {
+        DateTime start = DateTime.ParseExact(startStr, DateTimeFormat, CultureInfo.CurrentCulture);
+        DateTime end = DateTime.ParseExact(endStr, DateTimeFormat, CultureInfo.CurrentCulture);
+
+        Intervention intervention = GetIntervention(start, end);
+
+        Assert.Equal(dayHours, intervention.DayHours);
+        Assert.Equal(nightHours, intervention.NightHours);
+        Assert.Equal(specialHours, intervention.SpecialHours);
+        Assert.Equal(totalHours, intervention.TotalHours);
+    }
+
+    private static Intervention GetIntervention(DateTime startDateTime, DateTime endDateTime)
+    {
+        return new Intervention
+        {
+            StartDateTime = startDateTime,
+            EndDateTime = endDateTime,
+            Id = string.Empty,
+            Number = 34,
+            InterventionType = EInterventionType.Fire,
+            Title = string.Empty,
+            UnitAmount = 7.92,
+        };
+    }
+}
