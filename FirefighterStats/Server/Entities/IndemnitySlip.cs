09a2931d1fab0bc8d042f33cc3c8ce37089107b3
syukoGit <syukodev@outlook.com>
2023-02-03 17:24:35 +0100

    Add IdemnitySlip entities

---
+++ FirefighterStats/Server/Entities/IndemnitySlip.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.Server" file="IndemnitySlip.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Server.Entities;
+
+using System.ComponentModel.DataAnnotations;
+using FirefighterStats.Server.Entities.FirefighterActivities;
+using FirefighterStats.Shared.Utils;
+using JetBrains.Annotations;
+
+[UsedImplicitly]
+public class IndemnitySlip
+{
+    public IEnumerable<Activity>? Activities { get; set; }
+
+    [Key]
+    public required string Id { get; set; }
+
+    public IEnumerable<Intervention>? Interventions { get; set; }
+
+    public required EMonth Month { get; set; }
+
+    public required int Year { get; set; }
+}
---
+++ FirefighterStats/Shared/Utils/EMonth.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.Shared" file="EMonth.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Shared.Utils;
+
+public enum EMonth
+{
+    January,
+
+    February,
+
+    March,
+
+    April,
+
+    May,
+
+    June,
+
+    July,
+
+    August,
+
+    September,
+
+    October,
+
+    November,
+
+    December,
+}
