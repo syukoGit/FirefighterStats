eb24831d334f71ae592cee94ea14a921a0ccd53d
syukoGit <syukodev@outlook.com>
2023-02-03 15:32:53 +0100

    Fix - Add EInterventionType

---
+++ FirefighterStats/Shared/IndemnitySlip/FirefighterActivities/EInterventionType.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.Shared" file="EInterventionType.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Shared.IndemnitySlip.FirefighterActivities;
+
+using System.ComponentModel.DataAnnotations;
+
+public enum EInterventionType
+{
+    [Display(Name = "Operation diverse")]
+    OtherOperation,
+
+    [Display(Name = "SAP")]
+    PersonalAssistance,
+
+    [Display(Name = "Feu")]
+    Fire,
+
+    [Display(Name = "AVP")]
+    // Accident on Public Road
+    Apr,
+
+    [Display(Name = "Feu & AVP")]
+    FireAndApr,
+}
