324b4cdd51c73eb0a89d538157505b18940b53e0
syukoGit <syukodev@outlook.com>
2023-02-03 15:29:40 +0100

    Create an entity which represents an activity

---
+++ FirefighterStats/Server/Entities/FirefighterActivities/Activity.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats.Server" file="Activity.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Server.Entities.FirefighterActivities;
+
+using FirefighterStats.Shared.Utils;
+using JetBrains.Annotations;
+
+[UsedImplicitly]
+public class Activity : FirefighterActivity
+{
+    /// <inheritdoc />
+    public override double Amount => Math.Round(DurationInHours * Rate * UnitAmount, 2);
+
+    public double DurationInHours => (EndDateTime - StartDateTime).TotalHours;
+
+    public required Percentage Rate { get; set; }
+}
