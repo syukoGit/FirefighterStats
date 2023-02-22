cb3b5526ab796e1bf540c1b2f818645172cd80af
syukoGit <syukodev@outlook.com>
2023-02-22 21:41:42 +0100

    feat: create the main layout
    Create the main layout to App and add a navigation menu.
    
    BREAKING CHANGE: install react-router-dom and react-bootstrap-icons

--- FirefighterStats/App/src/App.tsx
+++ FirefighterStats/App/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter, Router } from 'react-router-dom';
+import './App.scss';
+import NavMenu from './components/NavMenu';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app'>
+            <header className='app__header'>
+                <div className='app__header__logo'>
+                    <img src='logo192.png' alt='logo' />
+                    <p>FirefighterStats</p>
+                </div>
+                <div>
+                    <p>TODO: LoginDisplay</p>
+                </div>
+            </header>
+
+            <div className='app__body'>
+                <BrowserRouter>
+                    <div className='app__body__sidebar'>
+                        <NavMenu />
+                    </div>
+
+                    <main className='app__body__content'>
+                        <p>TODO: Content</p>
+                    </main>
+                </BrowserRouter>
+
+                <p>TODO: Notification</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ FirefighterStats/App/src/components/NavMenu.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavMenu.scss';
+import { ArrowBarLeft, ArrowBarRight, House, Newspaper, PersonLinesFill } from 'react-bootstrap-icons';
+
+const NavMenu = () => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const toggleMenu = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    let navMenuClass = 'nav-menu';
+    if (isCollapsed) {
+        navMenuClass += ' nav-menu--collapsed';
+    }
+
+    return (
+        <div className={navMenuClass}>
+            <nav>
+                <NavLink className='nav-menu__item' to='/'>
+                    <House />
+                    <p>Home</p>
+                </NavLink>
+                <NavLink className='nav-menu__item' to='firefighter'>
+                    <PersonLinesFill />
+                    <p>Firefighters</p>
+                </NavLink>
+                <NavLink className='nav-menu__item' to='indemnity-slips'>
+                    <Newspaper />
+                    <p>Indemnity Slips</p>
+                </NavLink>
+            </nav>
+
+            <button className='nav-menu__button' onClick={toggleMenu} role='presentation' title='Collapse menu' type='button'>
+                {isCollapsed ? <ArrowBarRight /> : <ArrowBarLeft />}
+            </button>
+        </div>
+    );
+};
+
+export default NavMenu;
