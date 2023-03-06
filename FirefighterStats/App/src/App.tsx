8151fcae18dead208f78e94c3c827454d60c8979
syukoGit <syukodev@outlook.com>
2023-03-06 15:06:57 +0100

    refactor(notification): Move notifications provider
    Move notifications provider to index.tsx

--- FirefighterStats/App/src/App.tsx
+++ FirefighterStats/App/src/App.tsx
@@ -2,35 +2,32 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import NavMenu from './components/NavMenu';
 import NotificationList from './components/notifications/NotificationList';
-import { NotificationsProvider } from './utils/contexts/NotificationsContext';
 
 const App = () => (
     <div className='app'>
-        <NotificationsProvider>
-            <header className='app__header'>
-                <div className='app__header__logo'>
-                    <img src='logo192.png' alt='logo' />
-                    <p>FirefighterStats</p>
-                </div>
-                <div>
-                    <p>TODO: LoginDisplay</p>
-                </div>
-            </header>
-
-            <div className='app__body'>
-                <BrowserRouter>
-                    <div className='app__body__sidebar'>
-                        <NavMenu />
-                    </div>
-
-                    <main className='app__body__content'>
-                        <p>TODO: Content</p>
-                    </main>
-                </BrowserRouter>
-
-                <NotificationList />
+        <header className='app__header'>
+            <div className='app__header__logo'>
+                <img src='logo192.png' alt='logo' />
+                <p>FirefighterStats</p>
             </div>
-        </NotificationsProvider>
+            <div>
+                <p>TODO: LoginDisplay</p>
+            </div>
+        </header>
+
+        <div className='app__body'>
+            <BrowserRouter>
+                <div className='app__body__sidebar'>
+                    <NavMenu />
+                </div>
+
+                <main className='app__body__content'>
+                    <p>TODO: Content</p>
+                </main>
+            </BrowserRouter>
+
+            <NotificationList />
+        </div>
     </div>
 );
 
--- FirefighterStats/App/src/index.tsx
+++ FirefighterStats/App/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { NotificationsProvider } from './utils/contexts/NotificationsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <NotificationsProvider>
+            <App />
+        </NotificationsProvider>
     </React.StrictMode>
 );
 
