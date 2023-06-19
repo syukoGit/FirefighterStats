9e58e81535f657aadff1e0d8bed58da354eb9f8f
syukoGit <syukodev@outlook.com>
2023-06-19 21:14:09 +0200

    fix(app): display date times
    Remove seconds

--- FirefighterStats/App/src/utils/DateTime.ts
+++ FirefighterStats/App/src/utils/DateTime.ts
@@ -6,8 +6,17 @@ export const displayDateTimes = (element: Intervention | Activity) => {
     var endDateTime = new Date(element.endDateTime);
 
     if (startDateTime.getDate() === endDateTime.getDate()) {
-        return `${startDateTime.toLocaleDateString()} ${startDateTime.toLocaleTimeString()} - ${endDateTime.toLocaleTimeString()}`;
+        return `${startDateTime.toLocaleDateString()} ${startDateTime.toLocaleTimeString('fr-FR', {
+            hour: '2-digit',
+            minute: '2-digit',
+        })} - ${endDateTime.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })}`;
     } else {
-        return `${startDateTime.toLocaleString()} - ${endDateTime.toLocaleString()}`;
+        return `${startDateTime.toLocaleString('fr-FR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        })} - ${endDateTime.toLocaleString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' })}`;
     }
 };
